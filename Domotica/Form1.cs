af23a9367e4e5ae7d9bc4a25da105a38a96bb10b
Botez Mario Cosmin <botezmariocosmin@gmail.com>
2022-10-10 10:42:26 +0200

    Add project files.

---
+++ Domotica/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Domotica
+{
+    public partial class Form1 : Form
+    {
+        private Telecomando t;
+        public Form1()
+        {
+            InitializeComponent();
+            t = new Telecomando("samsung", "a1", true, false, false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             
+        }
+    }
+}
---
+++ Domotica/Telecomando.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Domotica
+{
+    public struct modFunzionamento
+    {
+        public bool infrarossi;
+        public bool bluetooth;
+        public bool wifi;
+    }
+    internal class Telecomando
+    {
+        int canale;
+
+        private string produttore;
+        private string modello;
+        
+
+        public Telecomando(string produttore1,string modello1, bool infrarossi1, bool bluetooth1, bool wifi1)
+        { 
+            produttore = produttore1;
+            modello = modello1;
+
+            modFunzionamento m;
+            m.infrarossi = infrarossi1;
+            m.bluetooth = bluetooth1;
+            m.wifi = wifi1;
+        }
+
+        public int getCanale()
+        {
+            return canale;
+        }
+
+        public void setCanale(int number)
+        {
+            canale = number;        
+        }
+
+    }
+}
